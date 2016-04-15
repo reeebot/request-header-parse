251a43f30a32623f05c779d1e82adfbc9a8a4f53
Tim Meijome <tim.meijome@gmail.com>
2016-04-15 13:54:51 -0600

    proxy fix

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-var express = require('express')
-var app = express()
-var useragent = require('useragent')
-var port = process.env.PORT || 8080
+var express = require('express');
+var app = express();
+var useragent = require('useragent');
+var port = process.env.PORT || 8080;
 
-
+app.enable('trust proxy');
 app.get('/*', function(req, res) {
-	var ipadd = req.ip.replace(/^.*:/, '')
-    var lang = req.headers["accept-language"].replace(/,.*/, '')
-    var agent = useragent.parse(req.headers['user-agent'])
-    var soft = agent.os.toString()
+	var ipadd = req.ip.replace(/^.*:/, '');
+    var lang = req.headers["accept-language"].replace(/,.*/, '');
+    var agent = useragent.parse(req.headers['user-agent']);
+    var soft = agent.os.toString();
     
 	var jsonstring = {
         ipaddress : ipadd,
         language : lang,
         software: soft
-    }
-    res.send(JSON.stringify(jsonstring))
+    };
+    res.send(JSON.stringify(jsonstring));
 });
 
 app.listen(port, function() {
-	console.log("server running")
+	console.log("server running");
 })
